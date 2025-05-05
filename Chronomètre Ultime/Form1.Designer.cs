92017842646c90a36414912de30337b8f2f4c662
hugschweize <hugschweize@PAPP-N121-M910>
2025-05-05 11:43:36 +0200

    Ajoutez des fichiers projet.

---
+++ Chronomètre Ultime/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace Chronomètre_Ultime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Chrono = new System.Windows.Forms.Timer(this.components);
+            this.label12 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.StopConvertion = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(237, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chrono : 0s";
+            this.label1.TextChanged += new System.EventHandler(this.label1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(330, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Changer : Secondes";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(248, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(446, 256);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 47);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(357, 270);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(58, 20);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Reset";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(357, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Color :\r\nBlack\r\n";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label3.Location = new System.Drawing.Point(707, 431);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Hugo Schweizer";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(721, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Convertir :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(707, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "--> Année";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(735, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Extra";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(707, 76);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "--> Miliesecondes";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(707, 52);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "--> Minutes";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(710, 32);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "--> Heures";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(709, 146);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "--> Mois (30j)";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(711, 168);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "--> Jours";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(691, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(108, 187);
+            this.panel1.TabIndex = 15;
+            // 
+            // Chrono
+            // 
+            this.Chrono.Enabled = true;
+            this.Chrono.Interval = 1000;
+            this.Chrono.Tick += new System.EventHandler(this.Chrono_Tick);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(251, 137);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(179, 31);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "Convertion :--";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.ForeColor = System.Drawing.Color.Black;
+            this.label14.Location = new System.Drawing.Point(731, 192);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(29, 13);
+            this.label14.TabIndex = 18;
+            this.label14.Text = "Stop";
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // StopConvertion
+            // 
+            this.StopConvertion.Enabled = true;
+            this.StopConvertion.Interval = 1;
+            this.StopConvertion.Tick += new System.EventHandler(this.StopConvertion_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(446, 340);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(82, 20);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label13.Location = new System.Drawing.Point(439, 324);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(98, 13);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "Nombre de décimal";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer Chrono;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Timer StopConvertion;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Chronomètre Ultime/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chronomètre_Ultime
+{
+    public partial class Form1 : Form
+    {
+        double secondes = 3700;
+        bool chrono = true;
+        int virguls = 3;
+        double arrondie;
+        double converted = 0;
+        string CNT = "-";
+        bool convertion = false;
+        string couleurChrono = "Black";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private async void Chrono_Tick(object sender, EventArgs e)
+        {
+            if (chrono == true)
+            {
+                secondes++;
+                label1.Text = "Chrono : " + secondes + "S";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                chrono = false;
+                label1.Font = new Font(label1.Font, label1.Font.Style | FontStyle.Italic);
+                label1.Text = "❄️ Chrono : " + secondes + "s ❄️";
+                label1.ForeColor = Color.LightBlue;
+                label1.Location = new Point(183, 168);
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+                chrono = true;
+                label1.Font = new Font(label1.Font, label1.Font.Style & ~FontStyle.Italic);
+                label1.Text = "Chrono : " + secondes + "S";
+                Couleur_label1();
+                label1.Location = new Point(237, 168);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult rn = MessageBox.Show("Voulez vous vraiment Réinitialiser la valeur du chrono ?", "Reset", MessageBoxButtons.YesNo);
+            if (rn == DialogResult.Yes)
+            {
+                secondes = 0;
+                MessageBox.Show("Valeur du chrono Réinitialisée !");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            DialogResult rn = MessageBox.Show("Quel Couleur voulez vous ?\nYes = Bleu\nNo = Rouge\nCancel = + de couleur", "Couleur", MessageBoxButtons.YesNoCancel);
+            if (rn == DialogResult.Yes)
+            {
+                couleurChrono = "Blue";
+                Couleur_label1();
+            }
+            if (rn == DialogResult.No)
+            {
+                couleurChrono = "Red";
+                Couleur_label1();
+            }
+            if (rn == DialogResult.Cancel)
+            {
+                DialogResult ra = MessageBox.Show("Quel Couleur voulez vous ?\nYes = Vert\nNo = Violet\nCancel = + de couleur", "Couleur", MessageBoxButtons.YesNoCancel);
+                if (ra == DialogResult.Yes)
+                {
+                    couleurChrono = "Green";
+                    Couleur_label1();
+                }
+                if (ra == DialogResult.No)
+                {
+                    couleurChrono = "Purple";
+                    Couleur_label1();
+                }
+                if (ra == DialogResult.Cancel)
+                {
+                    DialogResult re = MessageBox.Show("Quel Couleur voulez vous ?\nYes = Jaune\nNo = Noir\nCancel = Annuler", "Couleur", MessageBoxButtons.YesNoCancel);
+                    if (re == DialogResult.Yes)
+                    {
+                        couleurChrono = "Yellow";
+                        Couleur_label1();
+                    }
+                    if (re == DialogResult.No)
+                    {
+                        couleurChrono = "Black";
+                        Couleur_label1();
+                    }
+                    if (re == DialogResult.Cancel)
+                    {
+
+                    }
+                }
+            }
+        }
+        private void Couleur_label1()
+        {
+            if (chrono == false)
+            {
+                label1.ForeColor = Color.LightBlue;
+            }
+            if (couleurChrono == "Black" && chrono == true)
+            {
+                label1.ForeColor = Color.Black;
+            }
+            if (couleurChrono == "Red" && chrono == true)
+            {
+                label1.ForeColor = Color.Red;
+            }
+            if (couleurChrono == "Yellow" && chrono == true)
+            {
+                label1.ForeColor = Color.Yellow;
+            }
+            if (couleurChrono == "Purple" && chrono == true)
+            {
+                label1.ForeColor = Color.Purple;
+            }
+            if (couleurChrono == "Green" && chrono == true)
+            {
+                label1.ForeColor = Color.Green;
+            }
+            if (couleurChrono == "Blue" && chrono == true)
+            {
+                label1.ForeColor = Color.Blue;
+            }
+            
+
+            
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            convertion = true;
+            CNT = "H";
+            convertion_on();
+        }
+        private async void convertion_on()
+        {
+            virguls = Convert.ToInt16(textBox1.Text);
+            if (convertion == true)
+            {
+                if (CNT == "H")
+                {
+                    converted = secondes / 3600;
+                    arrondie = Math.Round(converted, virguls);
+
+                }
+            }
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+            DialogResult rn = MessageBox.Show("Voulez vous vraiment Réinitialiser la valeur de la convertion ?", "Reset", MessageBoxButtons.YesNo);
+            if (rn == DialogResult.Yes)
+            {
+                convertion = false;
+                CNT = "-";
+                label12.Text = "Convertion--";
+                MessageBox.Show("Valeur de la convertion Réinitialisée !");
+            }
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+            convertion = false;
+            CNT = "-";
+        }
+
+        private void StopConvertion_Tick(object sender, EventArgs e)
+        {
+
+            if (convertion == false)
+            {
+                label12.Text = "Convertion--";
+                CNT = "-";
+            }
+            if (CNT == "H")
+            {
+                label9.ForeColor = Color.SkyBlue;
+            }
+            else
+            {
+                label9.ForeColor = Color.Black;
+            }
+            if (CNT == "Min")
+            {
+                label8.ForeColor = Color.SkyBlue;
+            }
+            else
+            {
+                label8.ForeColor = Color.Black;
+            }
+            if (CNT == "Mil")
+            {
+                label7.ForeColor = Color.SkyBlue;
+            }
+            else
+            {
+                label7.ForeColor = Color.Black;
+            }
+            if (CNT == "Ans")
+            {
+                label5.ForeColor = Color.SkyBlue;
+            }
+            else
+            {
+                label5.ForeColor = Color.Black;
+            }
+            if (CNT == "Mois")
+            {
+                label10.ForeColor = Color.SkyBlue;
+            }
+            else
+            {
+                label10.ForeColor = Color.Black;
+            }
+            if (CNT == "J")
+            {
+                label11.ForeColor = Color.SkyBlue;
+            }
+            else
+            {
+                label11.ForeColor = Color.Black;
+            }
+
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+            CNT = "Min";
+            convertion_on();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            convertion = true;
+            CNT = "Mil";
+            convertion_on();
+            
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            convertion = true;
+            CNT = "J";
+            convertion_on();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            convertion = true;
+            CNT = "Mois";
+            convertion_on();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            convertion = true;
+            CNT = "Ans";
+            convertion_on();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Autorise uniquement les chiffres et les touches de contrôle (ex: backspace)
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // empêche l'entrée
+            }
+        }
+
+        private void label1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
